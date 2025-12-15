6ae360998047cb3a828d886928cfc826d481bb0c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-15 23:20:04 +0300

    Create part-x-0504-numpy-array-full1.py

---
+++ part-x-0504-numpy-array-full1.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+my_arr = np.full(7, 6, dtype="int")
+
+my_arr2 = np.full((6, 3), 5, dtype="float16")
+
+print(my_arr)
+
+print(my_arr2)
+
+# [6 6 6 6 6 6 6]
+
+# [[5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]
+#  [5. 5. 5.]]
