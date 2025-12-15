5f5e99aadb2f1edd1b6fae34fb944f291225f486
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-15 23:17:08 +0300

    Create part-x-0503-numpy-array-ones1.py

---
+++ part-x-0503-numpy-array-ones1.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+my_arr = np.ones(7, dtype="int")
+
+my_arr2 = np.ones((6, 3), dtype="float16")
+
+print(my_arr)
+
+print(my_arr2)
+
+# [1 1 1 1 1 1 1]
+
+# [[1. 1. 1.]
+#  [1. 1. 1.]
+#  [1. 1. 1.]
+#  [1. 1. 1.]
+#  [1. 1. 1.]
+#  [1. 1. 1.]]
